3cce26dcf3754287dc93736aa37ff04642d7423e
Vasant Singh Negi <Vasantnegi7@gmail.com>
2023-09-26 16:25:37 +0530

    DataTypes, variables

---
+++ LearningJavaScript/DataTypes.js
@@ -0,0 +1,13 @@
+/*
+Basic js data types
+int, bigInt, String, boolean, null -- it is an object , undefined 
+Declaring variables -- let, const
+*/
+
+let a = "Vasant";
+let b = "Singh";
+let c = "Negi";
+
+console.log("Vasant");
+// for printing n no of variables at one go in console.log
+console.table([a, b, c]);
