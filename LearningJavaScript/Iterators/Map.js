777345502e7e604efc060eb6994619a63880301a
Vasant Singh Negi <Vasantnegi7@gmail.com>
2023-10-26 17:14:41 +0530

    revision

--- LearningJavaScript/Iterators/Map.js
+++ LearningJavaScript/Iterators/Map.js
@@ -1,11 +1,12 @@
 /**
  * Map -- is similar to for each but return values
+ * it takes handler/arrow fn -- moves on to each element and do thing that is asked
  */
 
 const nums = [4, 5, 6, 71, 2];
 
 let mappedResult = nums.map((item) => {
-  return item;
+  return item + 1;
 });
 
 console.log(`Mapped Result ${mappedResult}`);
--- LearningJavaScript/Iterators/Reducer.js
+++ LearningJavaScript/Iterators/Reducer.js
@@ -5,8 +5,8 @@
 
 const nums = [1, 2, 3, 4, 5, 6, 7];
 
-let reducerResult = nums.reduce((acc, num) => {
-  return acc + num;
+let reducerResult = nums.reduce((acc, element) => {
+  return acc + element;
 }, 0);
 //0 here is starting value(acc) that we provide
 
